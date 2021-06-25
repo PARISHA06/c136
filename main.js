e974ed3c1a709c181d5d7987a69b173eac3d137d
PARISHA06 <77620938+PARISHA06@users.noreply.github.com>
2021-06-25 15:37:32 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,32 @@
+status="";
+video="";
+function setup()
+{
+    canvas = createCanvas(480, 380);
+    canvas.center();
+}
+
+function start()
+{
+    objectDetector = ml5.objectDetector("cocossd", modelLoaded);
+    document.getElementById("status").innerHTML="Status : Dectecting Objects";
+}
+
+function modelLoaded()
+{
+    console.log("modelLoaded");
+    status=true;
+    video.loop();
+    video.speed(1);
+    video.volume(1);
+}
+function preload()
+{
+    video = createVideo("video.mp4");
+    video.hide();
+}
+
+function draw()
+{
+    image(video, 0, 0, 480, 380);
+}
